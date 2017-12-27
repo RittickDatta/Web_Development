61181a06d5b075b034e9f70a178ce851995794f4
Rittick <drittick@gmail.com>
2017-12-27 15:29:34 -0800

    Node Refresher: nodemon

--- Node Sample/notes-node/app.js
+++ Node Sample/notes-node/app.js
@@ -1,10 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
-const notes = require('./notes.js');
+const _ = require('lodash');
 
-var res = notes.add(1, 2);
-console.log('Result', res);
-// var user = os.userInfo();
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
+const notes = require('./notes.js');
