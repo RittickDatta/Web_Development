1e74d8b89581704421d00415a227e72417321e5e
Rittick <drittick@gmail.com>
2017-12-27 15:40:20 -0800

    Node Refresher: command arguments

--- Node Sample/notes-node/app.js
+++ Node Sample/notes-node/app.js
@@ -4,3 +4,19 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes.js');
+
+var command = process.argv[2];
+console.log('Command:', command);
+
+if(command === 'add'){
+  console.log('Adding New Note');
+} else if (command === 'list'){
+  console.log('Lisiting All Notes');
+} else if(command === 'read'){
+  console.log('Reading Note');
+} else if(command === 'remove'){
+  console.log('Removing Note');
+}
+else {
+  console.log('Command Not Recognized');
+}
