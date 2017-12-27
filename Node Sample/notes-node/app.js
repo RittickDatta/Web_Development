e69874fcc34e5ef9fd98d292758503547013b3e2
Rittick <drittick@gmail.com>
2017-12-27 14:40:35 -0800

    Node Refresher - require, functions, var, const.

--- Node Sample/notes-node/app.js
+++ Node Sample/notes-node/app.js
@@ -1,5 +1,10 @@
-console.log('Starting app.');
+console.log('Starting app.js');
 
 const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
 
-fs.appendFile('greetings.txt', 'Hello World!');
+var res = notes.add(1, 2);
+console.log(res);
+// var user = os.userInfo();
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
