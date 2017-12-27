29801f9462e3e126a5ee3be71c08b399ea09726d
Rittick <drittick@gmail.com>
2017-12-27 14:47:26 -0800

    Node Refresher: package.json

--- Node Sample/notes-node/app.js
+++ Node Sample/notes-node/app.js
@@ -5,6 +5,6 @@ const os = require('os');
 const notes = require('./notes.js');
 
 var res = notes.add(1, 2);
-console.log(res);
+console.log('Result', res);
 // var user = os.userInfo();
 // fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
