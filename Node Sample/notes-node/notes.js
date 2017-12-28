2f0daad577654ba8ce08d7ec6469d7b90d7a01fd
Rittick <drittick@gmail.com>
2017-12-28 13:26:35 -0800

    Notes App: List Working

--- Node Sample/notes-node/notes.js
+++ Node Sample/notes-node/notes.js
@@ -31,7 +31,7 @@ var addNote = (title, body) => {
 };
 
 var getAll = () => {
-  console.log('Getting All Notes.');
+  return fetchNotes();
 };
 
 var getNote = (title) => {
---
+++ Node Sample/notes-node/playground/debugging.js
@@ -0,0 +1,9 @@
+var person = {
+    name: 'Rittick'
+}
+
+person.age = 25;
+
+person.name = 'Harshit';
+
+console.log(person);
