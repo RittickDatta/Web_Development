5ce4c8c6c062b607c72f36d3996360805179de0f
Rittick <drittick@gmail.com>
2017-12-28 18:55:54 -0800

    Note App Complete, Arrow Functions, 'this' binding and 'arguments' access

---
+++ Node Sample/notes-node/playground/arrow-function.js
@@ -0,0 +1,17 @@
+var square = (x) =>  x * x;
+
+console.log(square(9));
+
+var user = {
+  name:'Rittick',
+  sayHi: () => {
+    console.log(arguments);
+    console.log(`Hi. I am ${this.name}`);
+  },
+  sayHiAlt(){
+    console.log(arguments);
+    console.log(`Hi. I am ${this.name}`);
+  }
+};
+
+user.sayHiAlt(1, 2, 3);
