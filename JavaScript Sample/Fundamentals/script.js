3932190e9286504843dc06b5bf9b81db838b8541
Rittick <drittick@gmail.com>
2018-01-07 22:42:26 -0800

    JavaScript

---
+++ JavaScript Sample/Fundamentals/script.js
@@ -0,0 +1,51 @@
+//Variables
+/*
+var name = 'John';
+console.log(name);
+
+var lastName= 'Smith';
+console.log(lastName);
+
+var age=26;
+console.log(age);
+
+var aboveAge = true;
+console.log(aboveAge);
+
+
+// Variables 2
+
+var name = 'John';
+var age = 26;
+
+//console.log(name+age);
+//console.log(age+age);
+
+var job, isMarried;
+
+//console.log(job);
+
+job="Teacher";
+isMarried=false;
+
+console.log(name + ' is a ' + age + ' year old '+ job + '. Is he married? ' + isMarried + '.');
+
+age="thirty six";
+job="driver";
+
+console.log(name + ' is a ' + age + ' year old '+ job + '. Is he married? ' + isMarried + '.');
+
+var lastName = prompt('What is the last name?')
+console.log(lastName);
+
+alert(name + ' is a ' + age + ' year old '+ job + '. Is he married? ' + isMarried + '.');
+*/
+
+var now = 2016;
+var birthYear = now - 26;
+
+birthYear = now - 26 * 2;
+console.log(birthYear);
+
+var ageJohn
+
