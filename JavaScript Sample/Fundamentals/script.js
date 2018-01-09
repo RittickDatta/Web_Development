61ae27df8a296f9f964090e5a42ae3899879710b
Rittick <drittick@gmail.com>
2018-01-08 16:47:45 -0800

    JavaScript Refresher

--- JavaScript Sample/Fundamentals/script.js
+++ JavaScript Sample/Fundamentals/script.js
@@ -84,6 +84,90 @@ if(23 == "23"){
     console.log('Something...');
 }
 
+
+var age = 20;
+
+if(age <= 19){
+    console.log('John is a teenager.');
+}else if(age >= 20 && age < 30){
+ console.log('John is a young man');
+}
+ else {
+    console.log('John is a man.')
+}
+
+var job = 'techer';
+
+job = prompt('What do you do?');
+
+switch(job){
+    case 'teacher': console.log('I am a teacher.');break;
+    case 'driver': console.log('I am a driver');break;
+    case 'cop': console.log('I am a cop');
+    default: console.log('Who am I?');
+    
+}
+
+
+function calculateAge(yearOfBirth){
+    var age = 2018 - yearOfBirth;
+    return age;
+}
+
+age = calculateAge(1948);
+console.log(age);
+
+function yearsUnitlRetirement(name, year){
+    var age  = calculateAge(year);
+    var retirement = 65 - age;
+    if(retirement >= 0){
+        console.log(name +' retires in '+retirement+' years .');
+    } else{
+        console.log(name+" has already retired ");
+    }
+    
+}
+
+yearsUnitlRetirement('John', 1990);
+
+var someFun = function(par) {
+    
+}
+
+
+var names= ['Rittick', 'Harshit', 'Shivam'];
+console.log(names);
+
+var years = [1990, 1988, 1999];
+console.log(years);
+
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+
+names[1] = 'Ben';
+console.log(names);
+
+var john = ['John', 'Smith', 1990, 'teacher', false];
+
+john.push('Red');
+john.unshift('Mr.');
+john.pop();
+john.pop();
+john.shift();
+console.log(john);
+
+var index = john.indexOf(1990);
+console.log(index);
+
+if(john.indexOf('Driver') === -1){
+    console.log('John is not a teacher');
+}
 */
+
+
+
+
+
 
 
