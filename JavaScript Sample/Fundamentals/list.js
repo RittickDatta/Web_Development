d58043ba1cdaf424ba50f59e0593b054c85a00c7
Rittick <drittick@gmail.com>
2018-01-29 21:53:15 -0600

    JavaScript Refresher: TO-DO Updated with Delete and Refactored into Functions

--- JavaScript Sample/Fundamentals/list.js
+++ JavaScript Sample/Fundamentals/list.js
@@ -5,12 +5,33 @@ var input = prompt("What would you like to do?");
 
 while(input  !== "quit"){
 if(input === "list"){
-	console.log(todos);	
+	listToDOs();
+	
 } else if(input === "new"){
-	var newTodo = prompt("Enter new todo.");
-	todos.push(newTodo);
+	addToDO();
+} else if (input ==="delete"){
+	deleteTODO();
 }
 input = prompt("What would you like to do?");
 }
 
 console.log("OK, YOU QUIT THE APP!");
+
+function listToDOs(){
+console.log("**********")
+	todos.forEach(function(todo, i){
+		console.log(i+": "+todo);
+	});	
+}
+
+function addToDO(){
+	var newTodo = prompt("Enter new todo.");
+	todos.push(newTodo);
+	console.log("TODO ADDED");
+}
+
+function deleteTODO(){
+	var index = prompt("Enter index of item to delete");	
+	todos.splice(index, 1);
+	console.log("DELETED TODO");
+}
